7f7e44437cd9a7baa19ad4a889be9574d1804ca7
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-09 20:54:46 +0200

    component routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { LoginComponent } from './login/login.component';
+import { RegistryComponent } from './registry/registry.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { VideoComponent } from './video/video.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainpageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registry', component: RegistryComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
+    { path: 'video', component: VideoComponent },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#141414',
+    },},
   },
   plugins: [],
 }
