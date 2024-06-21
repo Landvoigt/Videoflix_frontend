4da67b2124ec8589eb4dd8a9c9ad06fc2cf7961c
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-22 00:54:24 +0200

    netflix copy mainpage

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject} from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
@@ -9,4 +9,76 @@ import { Component } from '@angular/core';
 })
 export class MainpageComponent {
 
+
+  elementRef = inject(ElementRef)
+
+
+  ngOnInit(): void {
+    const lineIds = ['line1', 'line2', 'line3'];
+    lineIds.forEach(id => this.scrollElementById(id, 500));
+  }
+
+
+  private scrollElementById(id: string, scrollAmount: number): void {
+    setTimeout(() => {
+      const element: HTMLElement = this.elementRef.nativeElement.querySelector(`#${id}`);
+      if (element) {
+        element.scrollLeft += scrollAmount;
+      }
+    }, 0);
+  }
+
+
+  scrollingRight1() {
+    const line1Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line1');
+    if (line1Element) {
+      line1Element.scrollLeft += 700;
+    }
+  }
+
+
+  scrollingLeft1() {
+    const line1Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line1');
+    if (line1Element) {
+      line1Element.scrollLeft += -700;
+    }
+  }
+
+
+  scrollingRight2() {
+    const line2Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line2');
+    if (line2Element) {
+      line2Element.scrollLeft += 700;
+    }
+  }
+
+
+  scrollingLeft2() {
+    const line2Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line2');
+    if (line2Element) {
+      line2Element.scrollLeft += -700;
+    }
+  }
+
+
+
+  scrollingRight3() {
+    const line3Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line3');
+    if (line3Element) {
+      line3Element.scrollLeft += 700;
+    }
+  }
+  
+  
+   scrollingLeft3() {
+    const line3Element: HTMLElement = this.elementRef.nativeElement.querySelector('#line3');
+    if (line3Element) {
+      line3Element.scrollLeft += -700;
+    }
+  }
+  
+
 }
+
+
+ 
