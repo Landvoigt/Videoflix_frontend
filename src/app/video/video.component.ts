94eb22ab51b3765fee4b185eb0f3cd3ed397e733
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-24 21:37:18 +0200

    video.js player

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef,Input, ViewChild, OnDestroy,OnInit  } from '@angular/core';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
 @Component({
   selector: 'app-video',
   standalone: true,
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
   templateUrl: './video.component.html',
   styleUrl: './video.component.scss'
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit, OnDestroy  {
+  @ViewChild('target', {static: true}) target: ElementRef ;
+
+
+ @Input() options: {
+  fluid: boolean,
+  aspectRatio: string,
+  autoplay: boolean,
+  sources: {
+      src: string,
+      type: string,
+  }[],
+};
+ 
+
+player: Player ;
+constructor(
+  private elementRef: ElementRef,
+) {}
+
+ngOnInit() {
+  this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
+    console.log('Player is ready');
+  });
+}
+
+ngOnDestroy() {
+  if (this.player) {
+    this.player.dispose();
+  }
+}
+
+
+
 
 }
+
+
+
+//gsutil cors set C:\Users\Laptop\Desktop\config\cors-config.json gs://videoflix-videos
+
+// [
+//   {
+//     "origin": ["http://localhost:4200"],
+//     "responseHeader": ["Content-Type", "Authorization"],
+//     "method": ["GET", "HEAD", "OPTIONS"],
+//     "maxAgeSeconds": 3600
+//   }
+// ]
