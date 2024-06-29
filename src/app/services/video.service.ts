4b48d49c51606e40a60424b1a6e4ae1080ddb70f
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-29 21:19:50 +0200

    video player not working and config folder for cors

---
+++ src/app/services/video.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+}
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,13 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+//import { CommonModule } from '@angular/common'; 
+//import { FormsModule } from '@angular/forms';
+// import { VideoService } from '../services/video.service';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { catchError, map } from 'rxjs/operators';
+// import videojs from 'video.js';
+
+
 
 @Component({
   selector: 'app-video',
@@ -7,19 +16,41 @@ import { Component} from '@angular/core';
   templateUrl: './video.component.html',
   styleUrl: './video.component.scss'
 })
-export class VideoComponent {
 
+export class VideoComponent implements OnInit {
+  videoUrl: string = '';
+
+  @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getVideoUrl('waterfall_1'); 
+  }
+
+  getVideoUrl(videoKey: string) {
+    const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}`;
+
+    this.http.get<any>(apiUrl).subscribe({
+      next: (data) => {
+        console.log('Response from server:', data);
+        if (data && data.video_url) {
+          this.videoUrl = data.video_url;
+          console.log('Updated videoUrl:', this.videoUrl); 
+        } else {
+          console.error('Invalid response format from server'); 
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching video URL:', error); 
+      }
+    });
+  }
 }
 
 
 
-//gsutil cors set C:\Users\Laptop\Desktop\config\cors-config.json gs://videoflix-videos
 
-// [
-//   {
-//     "origin": ["http://localhost:4200"],
-//     "responseHeader": ["Content-Type", "Authorization"],
-//     "method": ["GET", "HEAD", "OPTIONS"],
-//     "maxAgeSeconds": 3600
-//   }
-// ]
+
+
+
