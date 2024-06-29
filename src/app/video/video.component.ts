76e3a44030b4b2ea4d61dfb1e64217be75674d02
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-29 22:41:24 +0200

    videopayer is working but https://goo.gl/xX8pDD

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 //import { CommonModule } from '@angular/common'; 
 //import { FormsModule } from '@angular/forms';
 // import { VideoService } from '../services/video.service';
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 // import { Observable } from 'rxjs';
 // import { catchError, map } from 'rxjs/operators';
 // import videojs from 'video.js';
-
+import Hls from 'hls.js'; // Importiere hls.js
 
 
 @Component({
@@ -17,14 +17,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './video.component.scss'
 })
 
-export class VideoComponent implements OnInit {
+export class VideoComponent implements AfterViewInit {
   videoUrl: string = '';
-
-  @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef;
+  @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    
     this.getVideoUrl('waterfall_1'); 
   }
 
@@ -36,15 +36,35 @@ export class VideoComponent implements OnInit {
         console.log('Response from server:', data);
         if (data && data.video_url) {
           this.videoUrl = data.video_url;
-          console.log('Updated videoUrl:', this.videoUrl); 
+          console.log('Updated videoUrl:', this.videoUrl);
+          this.setupVideoPlayer();
         } else {
-          console.error('Invalid response format from server'); 
+          console.error('Invalid response format from server');
         }
       },
       error: (error) => {
-        console.error('Error fetching video URL:', error); 
+        console.error('Error fetching video URL:', error);
       }
     });
+  }
+  setupVideoPlayer() {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(this.videoUrl);
+      hls.attachMedia(this.videoPlayer.nativeElement);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        console.log('HLS manifest parsed');
+        this.videoPlayer.nativeElement.play();
+      });
+    } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+      this.videoPlayer.nativeElement.src = this.videoUrl;
+      this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+        console.log('Native HLS support, video loaded');
+        this.videoPlayer.nativeElement.play();
+      });
+    } else {
+      console.error('HLS is not supported in this browser');
+    }
   }
 }
 
