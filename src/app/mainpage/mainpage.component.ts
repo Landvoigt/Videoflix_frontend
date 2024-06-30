59aa20f50245aeaf9b5377587a384101da680322
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-06-30 11:53:47 +0200

    mainpage main-video

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,6 +1,9 @@
-import { Component, ElementRef, inject} from '@angular/core';
+import { ApplicationRef, Component, ElementRef, NgZone, OnInit, ViewChild, inject} from '@angular/core';
 import { NavigationService } from '../services/navigation.service';
 import { AuthService } from '../auth/auth.service';
+import Hls from 'hls.js'; 
+import { first } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mainpage',
@@ -9,17 +12,173 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
-export class MainpageComponent {
+export class MainpageComponent implements OnInit {
 
-  constructor(public navService: NavigationService, public authService: AuthService) {
+
+  videoUrl: string = '';
+  @ViewChild('videoPlayerStart', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
+
+
+  constructor(
+    public navService: NavigationService,
+    public authService: AuthService,
+    private http: HttpClient,
+    private ngZone: NgZone,
+    private appRef: ApplicationRef
+  ) {
 
   }
+
+
+  // ngAfterViewInit() {
+  //  // this.appRef.isStable.pipe(first(isStable => isStable)).subscribe(() => {
+  //     const resolution = this.getResolution();
+  //     console.log('Resolution: ', this.getResolution());
+  //     this.getVideoUrl('jellyfish', resolution);
+  //  // });
+  // }
+
+  // getResolution(): string {
+  //   const width = window.innerWidth;
+  //   if (width >= 1920) {
+  //     return '1080p';
+  //   } else if (width >= 1280) {
+  //     return '720p';
+  //   } else if (width >= 854) {
+  //     return '480p';
+  //   } else {
+  //     return '360p';
+  //   }
+  // }
+
+  // getVideoUrl(videoKey: string, resolution: string) {
+  //   const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
+
+  //   this.http.get<any>(apiUrl).subscribe({
+  //     next: (data) => {
+  //       console.log('Response from server:', data);
+  //       if (data && data.video_url) {
+  //         this.videoUrl = data.video_url;
+  //         console.log('Updated videoUrl:', this.videoUrl);
+  //         this.setupVideoPlayer();
+  //       } else {
+  //         console.error('Invalid response format from server');
+  //       }
+  //     },
+  //     error: (error) => {
+  //       console.error('Error fetching video URL:', error);
+  //     }
+  //   });
+  // }
+
+  // setupVideoPlayer() {
+  //   this.ngZone.runOutsideAngular(() => {
+  //     if (Hls.isSupported()) {
+  //       const hls = new Hls();
+  //       hls.loadSource(this.videoUrl);
+  //       hls.attachMedia(this.videoPlayer.nativeElement);
+  //       hls.on(Hls.Events.MANIFEST_PARSED, () => {
+  //         console.log('HLS manifest parsed');
+  //       });
+  //     } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+  //       this.videoPlayer.nativeElement.src = this.videoUrl;
+  //       this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+  //         console.log('Native HLS support, video loaded');
+  //       });
+  //     } else {
+  //       console.error('HLS is not supported in this browser');
+  //     }
+  //   });
+
+
+
+  // ngAfterView() {
+  //   const resolution = this.getResolution();
+  //   console.log('Resolution: ', this.getResolution());
+  //   this.getVideoUrl('jellyfish', resolution);
+  // }
+
+  getResolution(): string {
+    const width = window.innerWidth;
+    if (width >= 1920) {
+      return '1080p';
+    } else if (width >= 1280) {
+      return '720p';
+    } else if (width >= 854) {
+      return '480p';
+    } else {
+      return '360p';
+    }
+  }
+
+  getVideoUrl(videoKey: string, resolution: string) {
+    const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
+
+    this.http.get<any>(apiUrl).subscribe({
+      next: (data) => {
+        console.log('Response from server:', data);
+        if (data && data.video_url) {
+          this.videoUrl = data.video_url;
+          console.log('Updated videoUrl:', this.videoUrl);
+          this.setupVideoPlayer();
+        } else {
+          console.error('Invalid response format from server');
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching video URL:', error);
+      }
+    });
+  }
+
+  setupVideoPlayer() {
+    this.ngZone.runOutsideAngular(() => {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(this.videoUrl);
+        hls.attachMedia(this.videoPlayer.nativeElement);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          console.log('HLS manifest parsed');
+          this.videoPlayer.nativeElement.play();
+        });
+      } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+        this.videoPlayer.nativeElement.src = this.videoUrl;
+        this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+          console.log('Native HLS support, video loaded');
+          this.videoPlayer.nativeElement.play();
+        });
+      } else {
+        console.error('HLS is not supported in this browser');
+      }
+    });
+  }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   elementRef = inject(ElementRef)
 
 
   ngOnInit(): void {
+    const resolution = this.getResolution();
+    console.log('Resolution: ', this.getResolution());
+    this.getVideoUrl('jellyfish', '360p');
     const lineIds = ['line1', 'line2', 'line3'];
     lineIds.forEach(id => this.scrollElementById(id, 500));
   }
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, ApplicationRef, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
 //import { CommonModule } from '@angular/common'; 
 //import { FormsModule } from '@angular/forms';
 // import { VideoService } from '../services/video.service';
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 // import { catchError, map } from 'rxjs/operators';
 // import videojs from 'video.js';
 import Hls from 'hls.js'; // Importiere hls.js
+import { first } from 'rxjs';
 
 
 @Component({
@@ -17,19 +18,306 @@ import Hls from 'hls.js'; // Importiere hls.js
   styleUrl: './video.component.scss'
 })
 
+// export class VideoComponent implements AfterViewInit {
+//   videoUrl: string = '';
+//   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
+
+//   constructor(private http: HttpClient) {}
+
+//   ngAfterViewInit() {
+    
+//     this.getVideoUrl('scooter'); 
+//   }
+
+//   getVideoUrl(videoKey: string) {
+//     const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}`;
+
+//     this.http.get<any>(apiUrl).subscribe({
+//       next: (data) => {
+//         console.log('Response from server:', data);
+//         if (data && data.video_url) {
+//           this.videoUrl = data.video_url;
+//           console.log('Updated videoUrl:', this.videoUrl);
+//           this.setupVideoPlayer();
+//         } else {
+//           console.error('Invalid response format from server');
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Error fetching video URL:', error);
+//       }
+//     });
+//   }
+//   setupVideoPlayer() {
+//     if (Hls.isSupported()) {
+//       const hls = new Hls();
+//       hls.loadSource(this.videoUrl);
+//       hls.attachMedia(this.videoPlayer.nativeElement);
+//       hls.on(Hls.Events.MANIFEST_PARSED, () => {
+//         console.log('HLS manifest parsed');
+//         this.videoPlayer.nativeElement.play();
+//       });
+//     } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+//       this.videoPlayer.nativeElement.src = this.videoUrl;
+//       this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+//         console.log('Native HLS support, video loaded');
+//         this.videoPlayer.nativeElement.play();
+//       });
+//     } else {
+//       console.error('HLS is not supported in this browser');
+//     }
+//   }
+// }
+
+
+
+// export class VideoComponent implements AfterViewInit {    Ez ajo
+//   videoUrl: string = '';
+//   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
+
+//   constructor(private http: HttpClient) {}
+
+//   ngAfterViewInit() {
+//     const resolution = this.getResolution();
+//     console.log('getResolution()', this.getResolution());
+//     this.getVideoUrl('scooter', resolution);
+//   }
+
+//   getResolution(): string {
+//     const width = window.innerWidth;
+//     if (width >= 1920) {
+//       return '1080p';
+//     } else if (width >= 1280) {
+//       return '720p';
+//     } else if (width >= 854) {
+//       return '480p';
+//     } else {
+//       return '360p';
+//     }
+    
+//   }
+
+//   getVideoUrl(videoKey: string, resolution: string) {
+//     const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
+
+//     this.http.get<any>(apiUrl).subscribe({
+//       next: (data) => {
+//         console.log('Response from server:', data);
+//         if (data && data.video_url) {
+//           this.videoUrl = data.video_url;
+//           console.log('Updated videoUrl:', this.videoUrl);
+//           this.setupVideoPlayer();
+//         } else {
+//           console.error('Invalid response format from server');
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Error fetching video URL:', error);
+//       }
+//     });
+//   }
+
+//   setupVideoPlayer() {
+//     if (Hls.isSupported()) {
+//       const hls = new Hls();
+//       hls.loadSource(this.videoUrl);
+//       hls.attachMedia(this.videoPlayer.nativeElement);
+//       hls.on(Hls.Events.MANIFEST_PARSED, () => {
+//         console.log('HLS manifest parsed');
+//         this.videoPlayer.nativeElement.play();
+//       });
+//     } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+//       this.videoPlayer.nativeElement.src = this.videoUrl;
+//       this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+//         console.log('Native HLS support, video loaded');
+//         this.videoPlayer.nativeElement.play();
+//       });
+//     } else {
+//       console.error('HLS is not supported in this browser');
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+// export class VideoComponent implements AfterViewInit {
+//   videoUrl: string = '';
+//   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
+
+//   constructor(private http: HttpClient, private ngZone: NgZone) {}
+
+//   ngAfterViewInit() {
+//     const resolution = this.getResolution();
+//     this.getVideoUrl('scooter', resolution);
+//   }
+
+//   getResolution(): string {
+//     const width = window.innerWidth;
+//     if (width >= 1920) {
+//       return '1080p';
+//     } else if (width >= 1280) {
+//       return '720p';
+//     } else if (width >= 854) {
+//       return '480p';
+//     } else {
+//       return '360p';
+//     }
+//   }
+
+//   getVideoUrl(videoKey: string, resolution: string) {
+//     const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
+
+//     this.http.get<any>(apiUrl).subscribe({
+//       next: (data) => {
+//         console.log('Response from server:', data);
+//         if (data && data.video_url) {
+//           this.videoUrl = data.video_url;
+//           console.log('Updated videoUrl:', this.videoUrl);
+//           this.setupVideoPlayer();
+//         } else {
+//           console.error('Invalid response format from server');
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Error fetching video URL:', error);
+//       }
+//     });
+//   }
+
+//   setupVideoPlayer() {
+//     this.ngZone.runOutsideAngular(() => {
+//       if (Hls.isSupported()) {
+//         const hls = new Hls();
+//         hls.loadSource(this.videoUrl);
+//         hls.attachMedia(this.videoPlayer.nativeElement);
+//         hls.on(Hls.Events.MANIFEST_PARSED, () => {
+//           console.log('HLS manifest parsed');
+//           this.videoPlayer.nativeElement.play();
+//         });
+//       } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+//         this.videoPlayer.nativeElement.src = this.videoUrl;
+//         this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+//           console.log('Native HLS support, video loaded');
+//           this.videoPlayer.nativeElement.play();
+//         });
+//       } else {
+//         console.error('HLS is not supported in this browser');
+//       }
+//     });
+//   }
+// }
+
+
+// export class VideoComponent implements AfterViewInit {     //ez is jo, csak hiba 
+//   videoUrl: string = '';
+//   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
+  
+//   constructor(
+//     private http: HttpClient, 
+//     private ngZone: NgZone, 
+//     private appRef: ApplicationRef
+//   ) {}
+
+//   ngAfterViewInit() {
+//     this.appRef.isStable.pipe(first(isStable => isStable)).subscribe(() => {
+//       const resolution = this.getResolution();
+//       this.getVideoUrl('scooter', resolution);
+//     });
+//   }
+
+//   getResolution(): string {
+//     const width = window.innerWidth;
+//     if (width >= 1920) {
+//       return '1080p';
+//     } else if (width >= 1280) {
+//       return '720p';
+//     } else if (width >= 854) {
+//       return '480p';
+//     } else {
+//       return '360p';
+//     }
+//   }
+
+//   getVideoUrl(videoKey: string, resolution: string) {
+//     const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
+
+//     this.http.get<any>(apiUrl).subscribe({
+//       next: (data) => {
+//         console.log('Response from server:', data);
+//         if (data && data.video_url) {
+//           this.videoUrl = data.video_url;
+//           console.log('Updated videoUrl:', this.videoUrl);
+//           this.setupVideoPlayer();
+//         } else {
+//           console.error('Invalid response format from server');
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Error fetching video URL:', error);
+//       }
+//     });
+//   }
+
+//   setupVideoPlayer() {
+//     this.ngZone.runOutsideAngular(() => {
+//       if (Hls.isSupported()) {
+//         const hls = new Hls();
+//         hls.loadSource(this.videoUrl);
+//         hls.attachMedia(this.videoPlayer.nativeElement);
+//         hls.on(Hls.Events.MANIFEST_PARSED, () => {
+//           console.log('HLS manifest parsed');
+//           this.videoPlayer.nativeElement.play();
+//         });
+//       } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+//         this.videoPlayer.nativeElement.src = this.videoUrl;
+//         this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+//           console.log('Native HLS support, video loaded');
+//           this.videoPlayer.nativeElement.play();
+//         });
+//       } else {
+//         console.error('HLS is not supported in this browser');
+//       }
+//     });
+//   }
+// }
+
 export class VideoComponent implements AfterViewInit {
   videoUrl: string = '';
   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement>;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private ngZone: NgZone,
+    private appRef: ApplicationRef
+  ) {}
 
   ngAfterViewInit() {
-    
-    this.getVideoUrl('waterfall_1'); 
+    this.appRef.isStable.pipe(first(isStable => isStable)).subscribe(() => {
+      const resolution = this.getResolution();
+      console.log('Resolution: ', this.getResolution());
+      this.getVideoUrl('jellyfish', resolution);
+    });
   }
 
-  getVideoUrl(videoKey: string) {
-    const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}`;
+  getResolution(): string {
+    const width = window.innerWidth;
+    if (width >= 1920) {
+      return '1080p';
+    } else if (width >= 1280) {
+      return '720p';
+    } else if (width >= 854) {
+      return '480p';
+    } else {
+      return '360p';
+    }
+  }
+
+  getVideoUrl(videoKey: string, resolution: string) {
+    const apiUrl = `http://localhost:8000/get-video-url/?video_key=${videoKey}&resolution=${resolution}`;
 
     this.http.get<any>(apiUrl).subscribe({
       next: (data) => {
@@ -47,30 +335,24 @@ export class VideoComponent implements AfterViewInit {
       }
     });
   }
+
   setupVideoPlayer() {
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(this.videoUrl);
-      hls.attachMedia(this.videoPlayer.nativeElement);
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        console.log('HLS manifest parsed');
-        this.videoPlayer.nativeElement.play();
-      });
-    } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
-      this.videoPlayer.nativeElement.src = this.videoUrl;
-      this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
-        console.log('Native HLS support, video loaded');
-        this.videoPlayer.nativeElement.play();
-      });
-    } else {
-      console.error('HLS is not supported in this browser');
-    }
+    this.ngZone.runOutsideAngular(() => {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(this.videoUrl);
+        hls.attachMedia(this.videoPlayer.nativeElement);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          console.log('HLS manifest parsed');
+        });
+      } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
+        this.videoPlayer.nativeElement.src = this.videoUrl;
+        this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+          console.log('Native HLS support, video loaded');
+        });
+      } else {
+        console.error('HLS is not supported in this browser');
+      }
+    });
   }
 }
-
-
-
-
-
-
-
